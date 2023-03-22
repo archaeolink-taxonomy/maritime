2e75d672c371e47c46f2e5d3970cda18b77d3495
florianthiery <mail@fthiery.de>
2023-03-22 16:07:12 +0000

    Deploying to gh-pages from  @ 3a833ba1cc4e0f1d8c869546f3c27eeefcc97b5c 🚀

--- nomt_classtree.js
+++ nomt_classtree.js
@@ -1764,7 +1764,7 @@ var tree={
               "instancecount": 2
             },
             "http://www.w3.org/2000/01/rdf-schema#label": {
-              "instancecount": 10
+              "instancecount": 9
             },
             "http://www.w3.org/2004/02/skos/core#broader": {
               "instancecount": 1,
@@ -1778,7 +1778,7 @@ var tree={
               "instancecount": 1
             },
             "http://www.w3.org/2004/02/skos/core#prefLabel": {
-              "instancecount": 10
+              "instancecount": 9
             },
             "http://www.w3.org/ns/prov#wasAttributedTo": {
               "instancecount": 1
@@ -1806,7 +1806,7 @@ var tree={
             }
           }
         },
-        "instancecount": 36
+        "instancecount": 34
       },
       {
         "id": "http://data.archaeology.link/data/maritimethesaurus/activity_1001",
@@ -3501,14 +3501,14 @@ var tree={
               "instancecount": 1
             },
             "http://www.w3.org/2000/01/rdf-schema#member": {
-              "instancecount": 215,
-              "http://www.w3.org/2004/02/skos/core#Concept": 215,
+              "instancecount": 231,
+              "http://www.w3.org/2004/02/skos/core#Concept": 231,
               "http://www.w3.org/2000/01/rdf-schema#Class": 215
             }
           },
           "from": {}
         },
-        "instancecount": 217
+        "instancecount": 233
       }
     ]
   }
