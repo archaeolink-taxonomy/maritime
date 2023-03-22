7753dc8b556d08ceedf1cd2672fd7c8e66343640
florianthiery <mail@fthiery.de>
2023-03-22 14:33:15 +0000

    Deploying to gh-pages from  @ f653e4714595cf64c3bd2c593f12012fa8f4024a 🚀

--- nomt_classtree.js
+++ nomt_classtree.js
@@ -1736,6 +1736,12 @@ var tree={
         "text": "Concept (skos:Concept) [231]",
         "data": {
           "to": {
+            "http://archaeology.link/ontology#identifier_db": {
+              "instancecount": 1
+            },
+            "http://archaeology.link/ontology#origin_description": {
+              "instancecount": 1
+            },
             "http://creativecommons.org/ns#attributionName": {
               "instancecount": 1
             },
@@ -1755,34 +1761,24 @@ var tree={
               "instancecount": 1
             },
             "http://www.w3.org/1999/02/22-rdf-syntax-ns#type": {
-              "instancecount": 1
-            },
-            "http://www.w3.org/2000/01/rdf-schema#label": {
               "instancecount": 2
             },
-            "http://www.w3.org/2000/01/rdf-schema#subClassOf": {
-              "instancecount": 4,
-              "http://www.w3.org/2004/02/skos/core#Concept": 4,
-              "http://www.w3.org/2000/01/rdf-schema#Class": 4
+            "http://www.w3.org/2000/01/rdf-schema#label": {
+              "instancecount": 10
+            },
+            "http://www.w3.org/2004/02/skos/core#broader": {
+              "instancecount": 1,
+              "http://www.w3.org/2004/02/skos/core#Concept": 1
             },
             "http://www.w3.org/2004/02/skos/core#inScheme": {
               "instancecount": 1,
               "http://www.w3.org/2004/02/skos/core#ConceptScheme": 1
             },
-            "http://www.w3.org/2004/02/skos/core#narrower": {
-              "instancecount": 4,
-              "http://www.w3.org/2004/02/skos/core#Concept": 4,
-              "http://www.w3.org/2000/01/rdf-schema#Class": 4
-            },
             "http://www.w3.org/2004/02/skos/core#note": {
               "instancecount": 1
             },
             "http://www.w3.org/2004/02/skos/core#prefLabel": {
-              "instancecount": 2
-            },
-            "http://www.w3.org/2004/02/skos/core#topConceptOf": {
-              "instancecount": 1,
-              "http://www.w3.org/2004/02/skos/core#ConceptScheme": 1
+              "instancecount": 10
             },
             "http://www.w3.org/ns/prov#wasAttributedTo": {
               "instancecount": 1
@@ -1798,20 +1794,19 @@ var tree={
           "from": {
             "http://www.w3.org/2000/01/rdf-schema#member": {
               "instancecount": 0,
-              "http://www.w3.org/2004/02/skos/core#Collection": 1
+              "http://www.w3.org/2004/02/skos/core#Collection": 2
             },
-            "http://www.w3.org/2004/02/skos/core#broader": {
+            "http://www.w3.org/2000/01/rdf-schema#subClassOf": {
               "instancecount": 0,
-              "http://www.w3.org/2004/02/skos/core#Concept": 4,
-              "http://www.w3.org/2000/01/rdf-schema#Class": 4
+              "http://www.w3.org/2004/02/skos/core#Concept": 1
             },
-            "http://www.w3.org/2004/02/skos/core#hasTopConcept": {
+            "http://www.w3.org/2004/02/skos/core#narrower": {
               "instancecount": 0,
-              "http://www.w3.org/2004/02/skos/core#ConceptScheme": 1
+              "http://www.w3.org/2004/02/skos/core#Concept": 1
             }
           }
         },
-        "instancecount": 25
+        "instancecount": 36
       },
       {
         "id": "http://data.archaeology.link/data/maritimethesaurus/activity_1001",
@@ -3457,7 +3452,8 @@ var tree={
             },
             "http://www.w3.org/ns/prov#wasGeneratedBy": {
               "instancecount": 0,
-              "http://www.w3.org/2004/02/skos/core#Concept": 1
+              "http://www.w3.org/2004/02/skos/core#Concept": 1,
+              "http://www.w3.org/2000/01/rdf-schema#Class": 1
             }
           }
         },
