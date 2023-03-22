7f2e5212e431c9f2b5bd58fa1888ea97f2d63a03
florianthiery <mail@fthiery.de>
2023-03-22 17:22:34 +0000

    Deploying to gh-pages from  @ 101fd563c3ea2e57962c74963c18e05b94887511 🚀

--- nomt_classtree.js
+++ nomt_classtree.js
@@ -46,7 +46,7 @@ var tree={
         "id": "http://data.archaeology.link/data/maritimethesaurus/cs01",
         "parent": "http://www.w3.org/2004/02/skos/core#ConceptScheme",
         "type": "instance",
-        "text": "Navis.one Maritime Thesaurus (nomt:cs01)",
+        "text": "NAVISone Maritime Thesaurus (nomt:cs01)",
         "data": {}
       },
       {
@@ -1764,7 +1764,7 @@ var tree={
               "instancecount": 2
             },
             "http://www.w3.org/2000/01/rdf-schema#label": {
-              "instancecount": 10
+              "instancecount": 2
             },
             "http://www.w3.org/2004/02/skos/core#broader": {
               "instancecount": 1,
@@ -1778,7 +1778,7 @@ var tree={
               "instancecount": 1
             },
             "http://www.w3.org/2004/02/skos/core#prefLabel": {
-              "instancecount": 10
+              "instancecount": 2
             },
             "http://www.w3.org/ns/prov#wasAttributedTo": {
               "instancecount": 1
@@ -1806,7 +1806,7 @@ var tree={
             }
           }
         },
-        "instancecount": 36
+        "instancecount": 20
       },
       {
         "id": "http://data.archaeology.link/data/maritimethesaurus/activity_1001",
@@ -3452,8 +3452,7 @@ var tree={
             },
             "http://www.w3.org/ns/prov#wasGeneratedBy": {
               "instancecount": 0,
-              "http://www.w3.org/2004/02/skos/core#Concept": 1,
-              "http://www.w3.org/2000/01/rdf-schema#Class": 1
+              "http://www.w3.org/2004/02/skos/core#Concept": 1
             }
           }
         },
